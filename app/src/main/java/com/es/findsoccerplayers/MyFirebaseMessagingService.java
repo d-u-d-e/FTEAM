848f9e4d1ed5a498f6421cecc641a9d886edff09
Marco Mariotto <mari8.marco@gmail.com>
2020-08-16 12:34:52 +0200

    Update MyFirebaseMessagingService.java

--- app/src/main/java/com/es/findsoccerplayers/MyFirebaseMessagingService.java
+++ app/src/main/java/com/es/findsoccerplayers/MyFirebaseMessagingService.java
@@ -35,7 +35,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         //If the current user sent the message or this match is currently browsed by the user do nothing
         if(!ActivityLogin.currentUserID.equals(remoteMessage.getData().get("sender")) &&
-                openedMatch != null && !openedMatch.equals(remoteMessage.getData().get("match"))){
+                (openedMatch == null || !openedMatch.equals(remoteMessage.getData().get("match")))){
             final Intent intent = new Intent(this, ActivitySelectMatch.class);
             intent.putExtra("type", "notification");
             intent.setAction("onNotificationClicked");
